928a95776cc5660f5bfddc8b6e515b3ec03fa0d7
James Carr <james.r.carr@gmail.com>
2009-04-10 01:44:23 -0400

    added tag support

--- src/jquery-html5-shim.js
+++ src/jquery-html5-shim.js
@@ -1,5 +1,10 @@
 (function(){
-
+        function create_elements(){
+            var html5_elements = ['section', 'article', 'm', 'header', 'footer', 'nav'];
+            for (var i in html5_elements){
+               var el =  document.createElement(html_elements[i]);
+            }
+        };
 	function decorate_placeholder(){
 	$('input').each(function(){
 		var input = $(this);
@@ -19,6 +24,7 @@
 	};
 	$.html5 = function(){
 		decorate_placeholder();
+                create_elements();
 	};
 })();
 
